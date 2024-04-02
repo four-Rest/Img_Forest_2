66610c085d5e40dd36c3506ccf90e1d0e8a4d349
tallguy188 <cksghd4220@naver.com>
2024-04-02 23:59:45 +0900

    feat: BaseTime 추가 및 util function 추가
    BaseTime 추가, util function 추가

--- src/main/java/com/ll/demo/global/util/Ut.java
+++ src/main/java/com/ll/demo/global/util/Ut.java
@@ -10,4 +10,23 @@ public class Ut {
             return bool != null && !bool;
         }
     }
+
+
+    public static class str {
+
+        public static String lcfirst(String str) {
+            if (str == null || str.isEmpty()) {
+                return str;
+            }
+            return str.substring(0, 1).toLowerCase() + str.substring(1);
+        }
+
+        public static boolean hasLength(String string) {
+            return string != null && !string.trim().isEmpty();
+        }
+
+        public static boolean isBlank(String string) {
+            return !hasLength(string);
+        }
+    }
 }
