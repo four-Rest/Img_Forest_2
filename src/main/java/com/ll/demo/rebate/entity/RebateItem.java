55254ca4ab3480b770f29a26214edbdb214c9587
tallguy188 <cksghd4220@naver.com>
2024-04-18 20:52:23 +0900

    refactor: 제약조건 해제를 위해 ManyToOne으로 변경
    ManyToOne으로 연관관계 변경

--- src/main/java/com/ll/demo/rebate/entity/RebateItem.java
+++ src/main/java/com/ll/demo/rebate/entity/RebateItem.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 public class RebateItem extends BaseTime {
     private LocalDateTime eventDate; // 판매(구매)가 발생한 날짜
     private LocalDateTime rebateDate; // 정산일
-    @OneToOne
+    @ManyToOne
     @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private OrderItem orderItem; // 주문상품
 
